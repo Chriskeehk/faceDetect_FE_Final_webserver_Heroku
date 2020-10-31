31de2a65abc89bb9132a48017630999262be877c
unknown <chris@hkmouse.com>
2020-10-31 22:37:39 +0800

    Try Update

--- src/component/Signin/Signin.js
+++ src/component/Signin/Signin.js
@@ -49,7 +49,7 @@ class Signin extends React.Component {
 			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 			      <legend className="f2 fw6 ph0 mh0">Sign In</legend>
 			      <div className="mt3">
-			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email-</label>
 			        <input 
 			        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 			        	type="email" 
@@ -59,7 +59,7 @@ class Signin extends React.Component {
 			        	/>
 			      </div>
 			      <div className="mv3">
-			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="password">Password-</label>
 			        <input 
 			        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 			        type="password" 
