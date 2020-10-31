21a5d3d68b6c928fb8a57144481e8b82d60f8f3c
unknown <chris@hkmouse.com>
2020-11-01 00:06:59 +0800

    New Deployment

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -23,7 +23,7 @@ onPasswordChange = (event) => {
 }
 
 onSubmitRegister = () => {
-		fetch('http://localhost:3001/register', {
+		fetch('https://react.hkmouse.com/server/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
