627b607b72eab85671bd3f48d492b1f67a57b8f8
unknown <chris@hkmouse.com>
2020-11-07 22:51:45 +0800

    Final

--- src/component/Signin/Signin.js
+++ src/component/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://obscure-coast-05662.herokuapp.com/signin', {
+		fetch('https://facedetect-chriskeehk-server.herokuapp.com/signin',  {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
